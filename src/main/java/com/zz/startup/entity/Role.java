d38b6d85f82ba7b4610654deac06a4cb381ac3d5
yxb_1990 <657048548@qq.com>
2015-11-02 19:00:40 +0800

    重构pom文件

--- src/main/java/com/zz/startup/entity/Role.java
+++ src/main/java/com/zz/startup/entity/Role.java
@@ -1,61 +1,70 @@
 package com.zz.startup.entity;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.util.List;
 
 public class Role extends BaseEntity {
 
-	@NotEmpty
-	private String roleName;
-	@NotEmpty
-	private String roleCode;
-	private List<String> permissions;
-	private List<Authority> authorities;
-	
-	public String getRoleName() {
-		return roleName;
-	}
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
-	public String getRoleCode() {
-		return roleCode;
-	}
-	public void setRoleCode(String roleCode) {
-		this.roleCode = roleCode;
-	}
-	public List<String> getPermissions() {
-		return permissions;
-	}
-	public void setPermissions(List<String> permissions) {
-		this.permissions = permissions;
-	}
-	public List<Authority> getAuthorities() {
-		return authorities;
-	}
-	public void setAuthorities(List<Authority> authorities) {
-		this.authorities = authorities;
-	}
+    @NotEmpty
+    private String roleName;
+    @NotEmpty
+    private String roleCode;
+    private List<String> permissions;
+    @DBRef
+    private List<Authority> authorities;
 
-	@Override
-	public boolean equals(Object o) {
-		if(o == null){
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<String> permissions) {
+        this.permissions = permissions;
+    }
+
+    public List<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
             return false;
         }
-        if (o == this){
-           return true;
+        if (o == this) {
+            return true;
         }
-        if (getClass() != o.getClass()){
+        if (getClass() != o.getClass()) {
             return false;
         }
-        Role role = (Role)o;
+        Role role = (Role) o;
         return (role != null && role.getId().equals(getId()));
-	}
-	
-	public int hashCode() {
-		int hash = 7;
-		hash = 31 * hash + (null == getId() ? 0 : getId().hashCode());
-		return hash;
-	}
+    }
+
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (null == getId() ? 0 : getId().hashCode());
+        return hash;
+    }
 }
--- src/main/java/com/zz/startup/entity/User.java
+++ src/main/java/com/zz/startup/entity/User.java
@@ -1,8 +1,8 @@
 package com.zz.startup.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.util.List;
 
@@ -10,12 +10,13 @@ public class User extends BaseEntity {
 
     private static final long serialVersionUID = 6478593744952785316L;
     @NotEmpty(message = "姓名不能为空")
-    private String userName;            // 登录名
+    private String userName;
+    @Transient
     private String plainPassword;
     private String password;
     private String salt;
     private String email;
-    private String mobile;
+    @DBRef
     private List<Role> roleList;
     private List<String> permissions;
 
@@ -59,23 +60,12 @@ public class User extends BaseEntity {
         this.email = email;
     }
 
-    @Transient
-    @JsonIgnore
-
     public String getPlainPassword() {
         return plainPassword;
     }
 
     public void setPlainPassword(String plainPassword) {
         this.plainPassword = plainPassword;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
     }
 
     public List<String> getPermissions() {
